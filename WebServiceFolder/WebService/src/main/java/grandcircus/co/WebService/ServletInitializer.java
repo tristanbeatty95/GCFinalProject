9094a55947f18e9b4fa75360c5d9813d3bf2ea78
tristanbeatty95 <tristanbeatty@ymail.com>
2022-08-29 19:00:23 -0400

    Setting up the repo

---
+++ WebServiceFolder/WebService/src/main/java/grandcircus/co/WebService/ServletInitializer.java
@@ -0,0 +1,13 @@
+package grandcircus.co.WebService;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebServiceApplication.class);
+	}
+
+}
