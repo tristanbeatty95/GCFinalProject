657f9552e087da9513410c2183a907f9cd870ada
tim <tfierek357@gmail.com>
2022-09-01 12:00:03 -0400

    started dynamic month creation in controller

--- WebAppFolder/WebApp/src/main/java/grandcircus/co/WebApp/Controllers/WebController.java
+++ WebAppFolder/WebApp/src/main/java/grandcircus/co/WebApp/Controllers/WebController.java
@@ -1,8 +1,12 @@
 package grandcircus.co.WebApp.Controllers;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
@@ -21,13 +25,24 @@ public class WebController {
 //	}
 	
 	@RequestMapping("/")
-	public String homeRedirect() {
+	public String homeRedirect() { 
+		//TODO: Finish sending date information to view for dynamic month creation
+		//Tim: "I was working on this but something came up, will finish this evening"
+		Calendar c = Calendar.getInstance();
+		Date today = new Date();
+		
+		int y = c.get(Calendar.YEAR); 
+		int m = c.get(Calendar.MONTH);
+		int d = c.get(Calendar.DATE);
+		
+		
+		
 		return "redirect:/monthly-calendar";
 	}
 	
 	//Month will be default page
 	@RequestMapping("/monthly-calendar")
-	public String displayMonth(Model model) {
+	public String displayMonth(@RequestParam int year, @RequestParam int month, Model model) {
 
 		return "month";
 	}
